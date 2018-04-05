df10d87bdc4717a788e120360868c5effc6b2030
Meredith Nelson <nelsm796@newschool.edu>
2018-04-05 16:16:54 -0400

    added post six

---
+++ studio-practice/professional-posts/04-nhl-branding copy 2/assets/js/main.js
@@ -0,0 +1,18 @@
+var smallImageOne = document.querySelector('.smallImage.one')
+var smallImageTwo = document.querySelector('.smallImage.two')
+var smallImageThree = document.querySelector('.smallImage.three')
+
+
+var bigImage = document.querySelector('.bigImage')
+
+smallImageOne.addEventListener('mouseover', function () {
+  bigImage.src = smallImageOne.src
+})
+
+smallImageTwo.addEventListener('mouseover', function () {
+  bigImage.src = smallImageTwo.src
+})
+
+smallImageThree.addEventListener('mouseover', function () {
+  bigImage.src = smallImageThree.src
+})
