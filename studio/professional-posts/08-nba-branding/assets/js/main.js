42eb07b93ae3ed8268b8f268d877dbece1429162
Meredith Nelson <nelsm796@newschool.edu>
2018-04-28 15:30:14 -0400

    testing js

--- studio/professional-posts/08-nba-branding/assets/js/main.js
+++ studio/professional-posts/08-nba-branding/assets/js/main.js
@@ -25,3 +25,84 @@ videoPost.addEventListener('mouseover', function() {
 videoPost.addEventListener('mouseout', function() {
     videoPost.pause()
 })
+
+var goldenStateDiv = document.querySelector('#golden-state-button');
+var goldenStateInfoDiv = document.querySelector('#golden-state-info');
+var goldenStateArenaDiv = document.querySelector('#golden-state-arena');
+var goldenStateLeaugeDiv = document.querySelector('#golden-state-league');
+var goldenStateLocationDiv = document.querySelector('#golden-state-location');
+var goldenStateYearDiv = document.querySelector('#golden-state-year');
+
+var teamId = 134865;
+
+// Insert your actual API request URL below
+fetch('https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=' + teamId)
+    .then(function(response) {
+        // Get the response and format it to JSON
+        return response.json();
+    })
+    .then(function(jsonData) {
+        // log the data
+        console.log(jsonData);
+		  renderGoldenState(jsonData);
+    });
+
+
+function renderGoldenState(data) {
+	console.log(data.teams[0].strStadium);
+	goldenStateArenaDiv.innerText = data.teams[0].strStadium;
+  goldenStateLocationDiv.innerText= data.teams[0].strStadiumLocation;
+	goldenStateLeaugeDiv.innerText = data.teams[0].strLeague;
+  goldenStateYearDiv.innerText = data.teams[0].intFormedYear;
+}
+
+
+goldenStateDiv.addEventListener('mouseover', function () {
+	goldenStateInfoDiv.style.display = 'block';
+})
+
+
+goldenStateDiv.addEventListener('mouseout', function () {
+	goldenStateInfoDiv.style.display = 'none';
+})
+
+
+var clippersDiv = document.querySelector('#clippers-button');
+var clippersInfoDiv = document.querySelector('#clippers-info');
+var clippersArenaDiv = document.querySelector('#clippers-arena');
+var clippersLeaugeDiv = document.querySelector('#clippers-league');
+var clippersLocationDiv = document.querySelector('#clippers-location');
+var clippersYearDiv = document.querySelector('#clippers-year');
+
+var teamId = 134866;
+
+// Insert your actual API request URL below
+fetch('https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=' + teamId)
+    .then(function(response) {
+        // Get the response and format it to JSON
+        return response.json();
+    })
+    .then(function(jsonData) {
+        // log the data
+        console.log(jsonData);
+		  renderClippers(jsonData);
+    });
+
+
+function renderClippers(data) {
+	console.log(data.teams[0].strStadium);
+	clippersArenaDiv.innerText = data.teams[0].strStadium;
+  clippersLocationDiv.innerText= data.teams[0].strStadiumLocation;
+	clippersLeaugeDiv.innerText = data.teams[0].strLeague;
+  clippersYearDiv.innerText = data.teams[0].intFormedYear;
+}
+
+
+clippersDiv.addEventListener('mouseover', function () {
+  clippersInfoDiv.style.display = 'block';
+})
+
+
+clippersDiv.addEventListener('mouseout', function () {
+	clippersInfoDiv.style.display = 'none';
+})
